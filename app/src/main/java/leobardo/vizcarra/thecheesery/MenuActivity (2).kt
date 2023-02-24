a268c444f0f2af6fcd55e979ec9e21fe26424c3d
Leo Vizcarra <vizcarraleobardo@gmail.com>
2023-02-24 16:24:38 -0700

    Commit 2
    Branchout

---
+++ app/src/main/java/leobardo/vizcarra/thecheesery/MenuActivity (2).kt
@@ -0,0 +1,51 @@
+package leobardo.vizcarra.thecheesery
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        val btnColdDrinks: Button = findViewById(R.id.button_cold_drinks);
+        val btnHotDrinks:Button = findViewById(R.id.button_hot_drinks);
+        val btnSweets:Button = findViewById(R.id.button_sweets);
+        val btnSalties:Button = findViewById(R.id.button_salties);
+        val btnCombos:Button = findViewById(R.id.button_combos);
+        val btnCustom:Button = findViewById(R.id.button_custom);
+
+        btnColdDrinks.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnHotDrinks.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSweets.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSalties.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCombos.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCustom.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/leobardo/vizcarra/thecheesery/Product.kt
@@ -0,0 +1,6 @@
+package leobardo.vizcarra.thecheesery
+
+data class Product (var name:String,
+                    var image:Int,
+                    var description:String,
+                    var price:Double)
